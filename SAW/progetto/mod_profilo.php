20f41f99bdd9140c46609ed8718a44d2e1d057f1
Danomog <39589515+Danomog@users.noreply.github.com>
2019-04-15 15:54:45 +0200

    Add files via upload

--- SAW/progetto/mod_profilo.php
+++ SAW/progetto/mod_profilo.php
@@ -6,35 +6,38 @@
 <body>
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                 <h4 class="modal-title" style="text-align: center;">Cambia immagine del profilo</h4>
+                 <h4 class="modal-title" style="text-align: center;">Cambia elementi del profilo</h4>
             </div>			
             <div class="modal-body">
 
-
-                <form action="pop-up.php" method="post" id="login">
-                        <input type="image" src="img/profilo/1.png" class="imp">
-                        <input type="image" src="img/profilo/2.png" class="imp">
-                        <input type="image" src="img/profilo/3.png" class="imp">
-                        <input type="image" src="img/profilo/4.png" class="imp">
-                        <input type="image" src="img/profilo/5.png" class="imp">
-                        <input type="image" src="img/profilo/6.png" class="imp">
-                        <input type="image" src="img/profilo/7.png" class="imp">
-                        <input type="image" src="img/profilo/8.png" class="imp">
-                        <input type="image" src="img/profilo/9.png" class="imp">
-                        <input type="image" src="img/profilo/10.png" class="imp">
-                        <input type="image" src="img/profilo/11.png" class="imp">
-                        <input type="image" src="img/profilo/12.png" class="imp">
-                        <input type="image" src="img/profilo/13.png" class="imp">
-                        <input type="image" src="img/profilo/14.png" class="imp">
-                        <input type="image" src="img/profilo/15.png" class="imp">
-                        <input type="image" src="img/profilo/16.png" class="imp">
-                        <input type="image" src="img/profilo/17.png" class="imp">
-                        <input type="image" src="img/profilo/18.png" class="imp">
-                    <!--
+                <form action="pop-up.php" method="post" id="login" style="text-align: center;">
+                        <label for="username"> Username: </label> <br>
+                        <input type="text">
+                        <br><br>
+                        <label for="email"> Email: </label> <br>
+                        <input type="email">
+                        <br><br>
+                        <label for="username"> Nome: </label> <br>
+                        <input type="text">
+                        <br><br>
+                        <label for="username"> Cognome: </label> <br>
+                        <input type="text">
+                        <br><br>
+                        <label for="username"> Sesso: </label> <br>
+                        <select>
+						  <option value="null">------</option>
+						  <option value="m">maschio</option>
+						  <option value="f">femmina</option>
+						  <option value="a">altro</option>
+						</select>
+                        <br><br>
+                        <label for="username"> Età: </label> <br>
+                        <input type="text">
+                        <br><br>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Chiudi</button>
                         <button type="submit" class="btn btn-primary">Salva</button>
-                    </div>		-->	
+                    </div>		
 
                 </form>
             </div>			
